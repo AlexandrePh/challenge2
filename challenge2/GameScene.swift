31bd99b242756f3028a999638b2e44b047441625
Alexandre Philippi <alexandrephilippi@icloud.com>
2018-05-07 16:30:59 -0300

    initial

---
+++ challenge2/GameScene.swift
@@ -0,0 +1,18 @@
+//
+//  GameScene.swift
+//  challenge2
+//
+//  Created by alexandre philippi on 07/05/18.
+//  Copyright © 2018 alexandre philippi. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
---
+++ challenge2Tests/challenge2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  challenge2Tests.swift
+//  challenge2Tests
+//
+//  Created by alexandre philippi on 07/05/18.
+//  Copyright © 2018 alexandre philippi. All rights reserved.
+//
+
+import XCTest
+@testable import challenge2
+
+class challenge2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
